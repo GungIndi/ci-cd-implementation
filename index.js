ea88a25f44ff1e97fe888c636e40d3e883ecc1cd
GungIndi <gungindikusumaputra@gmail.com>
2023-12-24 15:34:50 +0800

    update index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello NODEEEE!\n'
+  const msg = 'Hello BANG!\n'
   res.end(msg);
 });
 
