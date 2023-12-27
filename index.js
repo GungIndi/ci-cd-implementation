4cf5074adcb3d8dd8298f110de378c66b515bbed
GungIndi <gungindikusumaputra@gmail.com>
2023-12-27 15:01:51 +0800

    finish (hopefully)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello! mjs\n'
+  const msg = 'Hello! DUNIA\n'
   res.end(msg);
 });
 
