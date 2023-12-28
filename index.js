b15d09bace45a8292212309ccd50ccedcb59b078
GungIndi <gungindikusumaputra@gmail.com>
2023-12-28 13:05:47 +0800

    fix pt16

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = '<h1> HELLO TEJA JAKDJAKSDahkjahkjdhaskhdkY</h1>\n'
+  const msg = '<h1> HAIII CICD NYA BERHASILL NIH GAISSS</h1>\n'
   res.end(msg);
 });
 
